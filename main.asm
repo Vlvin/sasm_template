a9ea7ae33c3fe6366c028cf0dbe1203ed6663919
Vlvin <ghostv.work@gmail.com>
2026-02-20 09:17:08 +0400

    small bugfixes of default program

--- main.asm
+++ main.asm
@@ -7,7 +7,7 @@ main: ; entry point
   GET_DEC 4, ebx ; get 4 byte num and put to ebx 
   mov ecx, eax
   add ecx, ebx
-  mul ecx, eax
+  mul ecx
   PRINT_DEC 4, ecx ; print 4 byte num from eax
   PRINT_CHAR 10 ; print 10 -> '\n'
   PRINT_DEC 4, ecx ; print 4 byte num from eax
